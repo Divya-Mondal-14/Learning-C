6fb1dc455a40da2f4931aa7ccd77d77991a1e9ea
Divya-Mondal-14 <divyamondal20027@gmail.com>
2024-03-19 21:52:35 +0530

    prac 1

---
+++ codechef/practice/PCL01problems_START01.c
@@ -0,0 +1,14 @@
+// Number Mirror
+// Write a program that takes a number 
+// N as the input, and prints it to the output.
+
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d" ,&a);
+    printf("%d" ,a);
+    return 0;
+	
+}
+
